f21ed694f1395048dff2c86585e0a3b9844a9988
Riocodex <onwukachibike@gmail.com>
2025-01-05 01:03:13 +0100

    added animation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
+import Customizer from './pages/Customizer'
+import Canvas from './canvas'
+import Home from './pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1 className='head-text'>Threejs</h1>
-    </div>
+    <main className='app transition-all ease-in'>
+
+      <Home/>
+      <Canvas/>
+      <Customizer/> 
+    </main>
   )
 }
 
