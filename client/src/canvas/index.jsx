519c59b41be2a229878cef6d662e8cca0a62ec4f
Riocodex <onwukachibike@gmail.com>
2025-01-07 14:36:26 +0100

    updated

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -2,9 +2,23 @@
 import { Canvas } from '@react-three/fiber'
 import { Environment, Center } from '@react-three/drei'
 
+import Shirt from './Shirt'
+import Camerarig from './Camerarig'
+import Backdrop from './Backdrop'
+
 const CanvasModel = () => {
   return (
-    <div>CanvasModel</div>
+    <Canvas>
+      <ambientLight intensity={0.5}/>
+      <Environment preset='city'/>
+
+      {/* <CameraRig>
+        <Backdrop/> */}
+        <Center>
+            <Shirt/>
+          </Center>
+      {/* </CameraRig> */}
+    </Canvas>
   )
 }
 
