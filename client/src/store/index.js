57b4ee5db8f656e00ff25859b0013de9f7378b73
Riocodex <onwukachibike@gmail.com>
2025-01-11 18:56:55 +0100

    updated

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,8 +5,8 @@ const state = proxy({
     color:"#EFBD48",
     isLogoTexture: true,
     isFullTexture: false,
-    logoDecal: './threejs.png',
-    fullDecal:'./threejs.png'
+    logoDecal: './me.jpg',
+    fullDecal:'./me.jpg'
 
 });
 
